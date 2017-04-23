2b13d4d2bc56c332c6b25b0dcbb99aee93ebe4c8
Masayuki Ono <mono0926@gmail.com>
2017-04-23 15:10:15 +0900

    Add library target

---
+++ NativePopup/NativePopup.h
@@ -0,0 +1,19 @@
+//
+//  NativePopup.h
+//  NativePopup
+//
+//  Created by mono on 2017/04/23.
+//  Copyright © 2017 mono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NativePopup.
+FOUNDATION_EXPORT double NativePopupVersionNumber;
+
+//! Project version string for NativePopup.
+FOUNDATION_EXPORT const unsigned char NativePopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NativePopup/PublicHeader.h>
+
+
