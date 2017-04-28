28ef71d91acd6f2ff560c53284f43b2b84f9b231
Masayuki Ono <mono0926@gmail.com>
2017-04-28 21:03:36 +0900

    Refactor

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -13,19 +13,6 @@ class ViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool { return true }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let imageView = UIImageView(image: UIImage(named: "home"))
-        imageView.contentMode = .scaleToFill
-        view.insertSubview(imageView, at: 0)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        imageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }
-
     @IBAction func customDidTap(_ sender: UIButton) {
         NativePopup.show(image: UIImage(named: "love")!,
                          title: "参考になった",
