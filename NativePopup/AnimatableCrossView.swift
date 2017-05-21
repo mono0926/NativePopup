8da1554613751d38837ec134f6647ef74bdcf7a2
Masayuki Ono <mono0926@gmail.com>
2017-05-21 09:30:42 +0900

    Split file

--- NativePopup/AnimatableCrossView.swift
+++ NativePopup/AnimatableCrossView.swift
@@ -1,24 +1,28 @@
 //
-//  AnimatableDoneView.swift
+//  AnimatableCrossView.swift
 //  NativePopup
 //
-//  Created by mono on 2017/05/20.
+//  Created by mono on 2017/05/21.
 //  Copyright © 2017 mono. All rights reserved.
 //
 
 import Foundation
 
-class AnimatableDoneView: UIView, HasAnimatableLayer {
+class AnimatableCrossView: UIView, HasAnimatableLayer {
 
     let animatableLayer = CAShapeLayer()
+
+    var duration: TimeInterval { return 0.4 }
+
     override func layoutSubviews() {
         super.layoutSubviews()
 
         let length = frame.width
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: length * 0.196, y: length * 0.527))
-        path.addLine(to: CGPoint(x: length * 0.47, y: length * 0.777))
-        path.addLine(to: CGPoint(x: length * 0.99, y: length * 0.25))
+        path.move(to: CGPoint(x: length * 0.1, y: length * 0.1))
+        path.addLine(to: CGPoint(x: length * 0.9, y: length * 0.9))
+        path.move(to: CGPoint(x: length * 0.1, y: length * 0.9))
+        path.addLine(to: CGPoint(x: length * 0.9, y: length * 0.1))
 
         animatableLayer.path = path.cgPath
         animatableLayer.fillColor = UIColor.clear.cgColor
